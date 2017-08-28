1068ccbde864cb51b6e4862ddec5309f0832d21d
3poitn14guy <sunshine.farm.nc@gmail.com>
2017-08-27 20:46:52 -0400

    update vegetables is now working...mostly
    curl request for update is successful except for updating comments. No
    comments are submitted to the db on a create or an update.  Need to
    look into how the table was created and if it can be modified

--- scripts/vegetables-update.sh
+++ scripts/vegetables-update.sh
@@ -11,14 +11,29 @@
 #   }'
 #
 # echo
-curl --include --request PATCH http://localhost:4741/vegetables/4 \
+curl --include --request PATCH http://localhost:4741/vegetables/8 \
   --header "Content-Type: application/json" \
   --data '{
     "vegetable": {
-      "name": "Corn",
+      "name": "Squash",
       "image": "http://i.imgur.com/IysO0qE.jpg",
       "comment": "4 rows planted"
     }
   }'
+
+  # API="${API_ORIGIN:-http://localhost:4741}"
+  # URL_PATH="/vegetables/$ID"
+  # curl "${API}${URL_PATH}" \
+  #   --include \
+  #   --request PATCH \
+  #   --header "Content-Type: application/json" \
+  #   --header "Authorization: Token token=BAhJIiU4ZTVhZTZlNTI5NWQ2OTFjOTI4ZTBmMWJlOTk3MWU1MwY6BkVG--07e25743e5fcd16db41b37f1d1f9ca95870ea429" \
+  #   --data '{
+  #     "vegetable": {
+  #       "name": "Corn",
+  #       "image": "http://i.imgur.com/IysO0qE.jpg",
+  #       "comment": "4 rows planted"
+  #     }
+  #   }'
 
 echo
