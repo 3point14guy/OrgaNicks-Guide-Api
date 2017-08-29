26b1669e3d9b3e879fb19346a98fc2f5e0834bc2
3poitn14guy <sunshine.farm.nc@gmail.com>
2017-08-29 13:16:59 -0400

    garden delete CURL now working
    created curl request for delete garden
    organized curl files to make tree cleaner

--- scripts/user/delete-veggies-as-user.sh
+++ scripts/user/delete-veggies-as-user.sh
@@ -4,4 +4,4 @@ curl --include --request DELETE http://localhost:4741/vegetables/$ID \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}" \
 
-  ech0
+  echo
